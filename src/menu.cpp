2c84883bd7ef63723e59c0160c8f9c95e430dfd9
Adriana <adriannalmeida0@gmail.com>
2023-11-29 17:34:36 +0000

    add src

---
+++ src/menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adriana on 11/29/23.
+//
+
+#include "menu.h"
---
+++ src/menu.h
@@ -0,0 +1,14 @@
+//
+// Created by adriana on 11/29/23.
+//
+
+#ifndef INC_2PROJETO_MENU_H
+#define INC_2PROJETO_MENU_H
+
+
+class menu {
+
+};
+
+
+#endif //INC_2PROJETO_MENU_H
