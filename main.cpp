a96cfa570d254e6c18f4d4104be7e337026b7314
Martim Eloy <eloy.mtm@gmail.com>
2024-01-02 23:05:08 -0100

    Melhor menu de sempre

--- main.cpp
+++ main.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
 #include <random>
+#include <thread>
+#include <chrono>
 #include "src/menu.h"
 
 
-std::vector<std::string> phrases = {
-        "Why was the new IT official hospitalized? He accidentally touched the firewall.",
-        "Why are the insurance and premiums for all app developers enormously high? Because they are always crashing down!",
-        "There are 10 types of people in the world. Those who understand binary and those who don't.",
-        "Why are computers bad at boxing? Their barks are always worse than their bytes!",
-        "How does a computer science major pick up girls? \"Oh, shit thought this was Google!\"",
-        "What do you call it when computer science majors make fun of each other? Cyber Boolean.",
-        "What do you call a creepy Computer Science teacher? A PDF file.",
-        "What do you call a nap in computer science class? A CS-ta.",
-        "Why did the transgender person fail computer science? Because they were non-binary.",
-        "What do you call it when two computer scientists come simultaneously? An andgasm."
-};
+void loadingAnimation(int durationSeconds) {
+    int width = 50;
+    for (int i = 0; i < durationSeconds * 10; ++i) {
+        int pos = i % width;
+
+        std::cout << "\r[";
+        for (int j = 0; j < width; ++j) {
+            if (j < pos) std::cout << "=";
+            else if (j == pos) std::cout << ">";
+            else std::cout << " ";
+        }
+        std::cout << "] " << int((float)i / (durationSeconds * 10) * 100.0) << "% " << std::flush;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+    std::cout << "\r              \r";
+}
 
 int main() {
-    srand(static_cast<unsigned>(time(0)));
-    int index = rand() % phrases.size();
-    cout << phrases[index] << endl;
+    int durationSeconds = 5;
+    std::cout << "LOADING...\n";
+
+    // Cria uma thread para a animação de carregamento
+    std::thread loadingThread(loadingAnimation, durationSeconds);
+
+    // Executa o menu simultaneamente
     menu menu1;
     menu1.run();
+
+    // Aguarda a conclusão da thread de carregamento
+    loadingThread.join();
+
     return 0;
 }
